50d893e83938923fa1bb244d6bdfe62cee843a2f
Mario Cesar Gonzalez Francia <mariocego99@gmail.com>
2023-03-13 20:56:55 -0300

    tarea Framework

---
+++ api/models/Products.js
@@ -0,0 +1,21 @@
+/**
+ * Products.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: { type: "string", required: true },
+    description: { type: "string", required: true },
+    code: { type: "string", required: true },
+    thumbnail: { type: "string", required: true },
+    price: { type: "number", required: true },
+    stock: { type: "string", required: true },
+    timeStamp: { type: "string", required: true }
+  },
+
+};
+
